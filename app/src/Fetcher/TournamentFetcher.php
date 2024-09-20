b5b2596e6f65d4169c4962ac7ff11655e291c1cf
Egor Sytko <egorsytko@gmail.com>
2024-09-20 11:21:19 +0300

    add strict types

--- app/src/Fetcher/TournamentFetcher.php
+++ app/src/Fetcher/TournamentFetcher.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Fetcher;
 
 use App\Enum\Stage;
--- app/src/Service/MatchesGenerator/PlayOffGenerator.php
+++ app/src/Service/MatchesGenerator/PlayOffGenerator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service\MatchesGenerator;
 
 use App\Entity\Team;
